8f39ba8ec30f05756260822a1cdd13059f7bfae8
Jeffrey Parks <jeffrey.parks@genesisworld.com>
2022-06-09 17:17:17 -0500

    Correct issue where Creating EQApplicationPackets in zone/mob.cpp may not free pBuffer[]

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -1156,6 +1156,7 @@ void Mob::CreateSpawnPacket(EQApplicationPacket *app, Mob *ForWho)
 {
 	app->SetOpcode(OP_NewSpawn);
 	app->size = sizeof(NewSpawn_Struct);
+	safe_delete_array(app->pBuffer);
 	app->pBuffer = new uchar[app->size];
 	memset(app->pBuffer, 0, app->size);
 	auto ns = (NewSpawn_Struct *) app->pBuffer;
@@ -1173,7 +1174,7 @@ void Mob::CreateSpawnPacket(EQApplicationPacket *app, Mob *ForWho)
 void Mob::CreateSpawnPacket(EQApplicationPacket* app, NewSpawn_Struct* ns) {
 	app->SetOpcode(OP_NewSpawn);
 	app->size = sizeof(NewSpawn_Struct);
-
+	safe_delete_array(app->pBuffer);
 	app->pBuffer = new uchar[sizeof(NewSpawn_Struct)];
 
 	// Copy ns directly into packet
@@ -1365,6 +1366,7 @@ void Mob::CreateDespawnPacket(EQApplicationPacket* app, bool Decay)
 {
 	app->SetOpcode(OP_DeleteSpawn);
 	app->size = sizeof(DeleteSpawn_Struct);
+	safe_delete_array(app->pBuffer);
 	app->pBuffer = new uchar[app->size];
 	memset(app->pBuffer, 0, app->size);
 	DeleteSpawn_Struct* ds = (DeleteSpawn_Struct*)app->pBuffer;
